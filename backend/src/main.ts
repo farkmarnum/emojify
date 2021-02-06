64d0a2555d52799907aff848928d47808264fc3c
farkmarnum <mark.farnum@gmail.com>
2021-02-06 17:58:17 -0500

    trim output

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -26,36 +26,39 @@ app.post('/convert', (req: express.Request, res: express.Result) => {
 
   const words = input.replace(/\n/g, ' ').split(' ')
 
-  const result = words.reduce((acc: string, wordRaw: string) => {
-    const word = wordRaw.replace(/[^0-9a-zA-Z]/g, '').toLowerCase()
+  const result = words
+    .reduce((acc: string, wordRaw: string) => {
+      const word = wordRaw.replace(/[^0-9a-zA-Z]/g, '').toLowerCase()
 
-    const accNext = `${acc} ${wordRaw}`
+      const accNext = `${acc} ${wordRaw}`
 
-    const randomChoice = Math.random() * 100 <= density
-    const isTooCommon = commonWords.has(word)
+      const randomChoice = Math.random() * 100 <= density
+      const isTooCommon = commonWords.has(word)
 
-    const emojiFilter = shouldFilterEmojis
-      ? (option: string) => !isInappropriate(option)
-      : () => true
+      const emojiFilter = shouldFilterEmojis
+        ? (option: string) => !isInappropriate(option)
+        : () => true
 
-    const emojiOptions = Object.entries(emojiData[word] || {})
-      .filter(([option]) => emojiFilter(option))
-      .reduce(
-        (arr, [option, frequency]) => [
-          ...arr,
-          ...[...Array(frequency)].fill(option),
-        ],
-        [],
-      )
+      const emojiOptions = Object.entries(emojiData[word] || {})
+        .filter(([option]) => emojiFilter(option))
+        .reduce(
+          (arr, [option, frequency]) => [
+            ...arr,
+            ...[...Array(frequency)].fill(option),
+          ],
+          [],
+        )
 
-    if (isTooCommon || !randomChoice || emojiOptions.length === 0) {
-      return accNext
-    }
+      if (isTooCommon || !randomChoice || emojiOptions.length === 0) {
+        return accNext
+      }
 
-    const emojis = emojiOptions[Math.floor(Math.random() * emojiOptions.length)]
+      const emojis =
+        emojiOptions[Math.floor(Math.random() * emojiOptions.length)]
 
-    return `${accNext} ${emojis}`
-  }, '')
+      return `${accNext} ${emojis}`
+    }, '')
+    .trim()
 
   res.json({ result })
 })
